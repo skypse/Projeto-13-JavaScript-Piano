a1ade2169fd7c55c65352feb331e52d77eb11be4
skypse <skypse0136@gmail.com>
2024-04-24 20:28:24 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Escopo Global
+let pianoContainer = document.getElementsByClassName("piano-container");
+const base = "./audio/";
+window.onload = () => {
+  // keys
+  for (let index = 1; index <= 24; index++) {
+    let div = document.createElement("div");
+    div.classList.add("key", index <= 10 ? "black-key" : "white-key");
+    // dando play no audio quando clica
+    const number = index <= 9 ? "0" + index : index;
+    div.addEventListener("click", () => {
+      new Audio(`${base}key${number}.mp3`).play();
+    });
+    pianoContainer[0].appendChild(div);
+  }
+};
